46a1bec1196ae56b18429b49bd6e79e5b5cac036
Trym Borg <trym.w.borg@gmail.com>
2025-11-04 15:04:12 +0100

    mer tull i main

--- src/main.py
+++ src/main.py
@@ -5,14 +5,21 @@ from gui.listeners import on_click
 
 import pygame as p
 
-test = screen(500, 500, (on_click, ))
-ooga = button((50, 50), (25, 25), 
+test = screen(500, 500, (on_click, ), BGC=(40, 40, 40))
+
+cooga = textfield((200, 200), (0, 0), "ligma", color=(0, 0, 0, 0))
+
+ooga = button(
+        (50, 50), (175, 75), 
         lambda x: print("ooga"), 
-        color=(255, 255, 0)
+        color=(255, 255, 0),
+        text="start"
     )
+
 dooga = textfield((200, 200), (150, 150), "hello world", color=(255, 255, 0))
 
 test.add_component(ooga)
 test.add_component(dooga)
+test.add_component(cooga)
 
 test.run()
