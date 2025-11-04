083201a8059df4d562131f1422e0de46eaa1ea6a
Trym Borg <trym.w.borg@gmail.com>
2025-11-04 20:49:10 +0100

    Removed unnecessary imports and "pass" in functions

--- src/drone_movement/movement.py
+++ src/drone_movement/movement.py
@@ -1,13 +1,7 @@
-#import Tello3
-
-import threading 
-import socket
-import sys
 import time
 
 class drone:
     def __init__(self, sock, tello_address):
-
         self.sock = sock
         self.tello_address = tello_address
 
@@ -20,49 +14,37 @@ class drone:
 
     def takeoff(self):
         self.send("takeoff")
-        pass
   
     def land(self):
         self.send("land")
-        pass
 
     def flip(self):
         self.send("flip")
-        pass
 
     def forward(self, amount):
         self.send(f"forward {amount}")
-        pass
 
     def back(self, amount):
         self.send(f"back {amount}")
-        pass
 
     def left(self, amount):
         self.send(f"left {amount}")
-        pass
 
     def right(self, amount):
         self.send(f"right {amount}")
-        pass
 
     def up(self, amount):
         self.send(f"up {amount}")
-        pass
 
     def down(self, amount):
         self.send(f"down {amount}")
-        pass
 
     def rotation_clock(self, deg): #clockwise rotation
         self.send(f"cw {deg}")
-        pass
 
     def rotation_counter(self, deg): #counter clockwise rotation
         self.send(f"ccw {deg}")
-        pass
 
     def speed(self, cms): #cm per sekund
         self.send(f"speed {cms}")
-        pass
 
