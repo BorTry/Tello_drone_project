72590819c92c87f4dce7a30c087fdba954479399
Trym Borg <trym.w.borg@gmail.com>
2025-11-08 17:11:27 +0100

    added path to data_visualizer

--- src/data_visualizer/data_visualizer.py
+++ src/data_visualizer/data_visualizer.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 class data_vizualizer:
     def __init__(self):
+        self.path = __file__.replace("/src/data_visualizer/visualizer.py", "")
+        
         self.df = pd.read_csv(f"{self.path}/data/.csv")
         pass
 
