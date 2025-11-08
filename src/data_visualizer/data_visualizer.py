2496a271c9a6edd4f2587a31c104af8e7c871139
Marius <quantumfield03@gmail.com>
2025-11-08 17:56:24 +0100

    updated data_visualizer

--- src/data_visualizer/data_visualizer.py
+++ src/data_visualizer/data_visualizer.py
@@ -7,10 +7,20 @@ class vizualizer:
         print(self.path)
 
         self.filename = [i for i in os.listdir(f"{self.path}/data/") if i.endswith(".csv")]
-        print(self.filename)
+        print(f"Reading: {self.filename}")
 
-        self.df = pd.read_csv(f"{self.path}/data/{self.filename[0]}")
-        print(self.df)
+        self.text = pd.read_csv(f"{self.path}/data/{self.filename[-1]}") #leser nyeste csv-fila
+        #print(self.text)
+
+        # self.text = pd.read_csv(f"{self.path}/data/{"insert filename.csv"}") # csv override 
+        # print(self.text)
+
+    def clean_text(self):
+        self.cleaned_text = self.text.iloc[0, 0].strip() 
+        print(self.cleaned_text)
+        #return self.clean_text
 
 testing = vizualizer()
-vizualizer()
+#vizualizer()
+
+testing.clean_text()
