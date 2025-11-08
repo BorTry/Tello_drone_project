6d73a4742367c5bb88d2433eda91b3e4d1b580f7
Marius <quantumfield03@gmail.com>
2025-11-08 17:30:35 +0100

    data vizualiser csv read

--- src/data_visualizer/data_visualizer.py
+++ src/data_visualizer/data_visualizer.py
@@ -1,10 +1,16 @@
 import pandas as pd
+import os
 
-class data_vizualizer:
+class vizualizer:
     def __init__(self):
-        self.path = __file__.replace("/src/data_visualizer/visualizer.py", "")
-        
-        self.df = pd.read_csv(f"{self.path}/data/.csv")
-        pass
+        self.path = __file__.replace("/src/data_visualizer/data_visualizer.py", "")
+        print(self.path)
 
-    
+        self.filename = [i for i in os.listdir(f"{self.path}/data/") if i.endswith(".csv")]
+        print(self.filename)
+
+        self.df = pd.read_csv(f"{self.path}/data/{self.filename[0]}")
+        print(self.df)
+
+testing = vizualizer()
+vizualizer()
