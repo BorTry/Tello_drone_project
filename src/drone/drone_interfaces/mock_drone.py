1990bf738a926422f64148953a2d9f54f981a296
Trym Borg <trym.w.borg@gmail.com>
2025-11-09 20:04:50 +0100

    fixed outgoing data, so it reflects the real drone more

--- src/drone/drone_interfaces/mock_drone.py
+++ src/drone/drone_interfaces/mock_drone.py
@@ -1,45 +1,84 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 from time import sleep
 
+from threading import Thread
+
 IPS = 10 # iterations per second
 BUFFER_SIZE = 2048
+
+COMMAND_TO_FUNC = {
+    "takeoff" : "height",
+    "land": "height",
+    "forward": "x_acc",
+    "back": "x_acc",
+    "left": "z_acc",
+    "right": "z_acc",
+    "up": "y_acc",
+    "down": "y_acc",
+    "cw": "roll",
+    "ccw": "roll"
+}
+
+REVERSE_FUNCTIONS = {
+    "land"
+    "back"
+    "right"
+    "down"
+    "ccw"
+}
 
 class mock_drone:
     """
     simulates the recieving and sending of data for the Tello drone.
     """
     def __init__(self, shut_event):
-        self.x_acc = 0
-        self.y_acc = 0
-        self.z_acc = 0
-
-        self.roll = 0
-        self.yaw = 0
-        self.pitch = 0
-
-        self.height = 0
+        self.stats = {
+            "x_acc":0,
+            "y_acc":0,
+            "z_acc":0,
+            "roll":0,
+            "yaw":0,
+            "pitch":0,
+            "height":0,
+        }
 
         self.socket = socket(AF_INET, SOCK_DGRAM)
         self.socket.bind(("192.168.10.1", 8889)) # Tello IP address
 
         self.shut_event = shut_event
+        self.thread = None
 
     def send_data(self):
         self.socket.sendto(
-            "agx:%2d;agy:%2d;agz%2d;roll:%2d;yaw:%2d;pitch:%2d;height:%2d" % (self.x_acc, self.y_acc, self.z_acc, self.roll, self.yaw, self.pitch, self.height),
+            "agx:%2d;agy:%2d;agz%2d;roll:%2d;yaw:%2d;pitch:%2d;height:%2d" % (
+                self.x_acc, self.y_acc, self.z_acc, self.roll, self.yaw, self.pitch, self.height
+            ),
             ("0.0.0.0", 8890)
         )
 
     def run(self):
-        self.socket.settimeout(0.1)
+        def wrap():
+            self.socket.settimeout(0.1)
 
-        while (not self.shut_event.is_set()):
-            try:
-                data, a = self.socket.recvfrom(BUFFER_SIZE)
+            while (not self.shut_event.is_set()):
+                try:
+                    data, a = self.socket.recvfrom(BUFFER_SIZE)
 
-            except TimeoutError:
-                continue
+                    if not data:
+                        continue
 
-            self.send_data()
+                    data = data.decode(encoding="utf-8").split(" ")
 
-            sleep(60 / IPS)
+                    if (data[0] in REVERSE_FUNCTIONS):
+                        data[1] *= -1
+
+                    self.stats[COMMAND_TO_FUNC[data[0]]] += data[1]
+
+                except TimeoutError:
+                    continue
+
+                self.send_data()
+
+                sleep(60 / IPS)
+        
+        self.thread = Thread(target=wrap)
