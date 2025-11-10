0296e98e1e1f824e420476e631a446144d893ca7
Trym Borg <trym.w.borg@gmail.com>
2025-11-10 03:49:36 +0100

    made the mock drone send images over a udp connection

--- src/drone/drone_interfaces/mock_drone.py
+++ src/drone/drone_interfaces/mock_drone.py
@@ -1,11 +1,13 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 
 from threading import Thread
+from cv2 import VideoCapture, CAP_AVFOUNDATION, CAP_PROP_FRAME_WIDTH, CAP_PROP_FRAME_HEIGHT, CAP_PROP_FPS
 
 IPS = 10 # iterations per second
 BUFFER_SIZE = 2048
 
 COMMAND_TO_FUNC = {
+    "streamon": "streamon",
     "takeoff" : "height",
     "land": "height",
     "forward": "x_acc",
@@ -26,7 +28,14 @@ REVERSE_FUNCTIONS = {
     "ccw"
 }
 
-ADDRESS = "0.0.0.0"
+BIND_ADDRESS = "0.0.0.0"
+
+SEND_ADDRESS = "127.0.0.1"
+IMAGE_PORT = 11111
+
+WIDTH = 640
+HEIGHT = 480
+FPS = 30
 
 class mock_drone:
     """
@@ -41,15 +50,22 @@ class mock_drone:
             "yaw":0,
             "pitch":0,
             "height":0,
-            "battery":100
+            "battery":100,
         }
+        
+        self.streamOn = False
 
         self.socket = socket(AF_INET, SOCK_DGRAM)
-        self.socket.bind((ADDRESS, 8889)) # Tello IP address
+        self.socket.bind((BIND_ADDRESS, 8889)) # Tello IP address
         self.socket.settimeout(0.01)
 
         self.shut_event = shut_event
         self.thread = None
+
+        self.cap = VideoCapture(1, CAP_AVFOUNDATION)
+        self.cap.set(CAP_PROP_FRAME_WIDTH, WIDTH)
+        self.cap.set(CAP_PROP_FRAME_HEIGHT, HEIGHT)
+        self.cap.set(CAP_PROP_FPS, FPS)
 
     def send_data(self):
         stat_string = "agx:%d;agy:%d;agz:%d;roll:%d;yaw:%d;pitch:%d;height:%d;battery:%d" % (
@@ -63,6 +79,18 @@ class mock_drone:
             ("0.0.0.0", 8890)
         )
 
+    def send_image(self):
+        if not self.streamOn:
+            print("not streaming...")
+            return
+        
+        ok, image = self.cap.read()
+
+        if not ok:
+            return
+    
+        self.proc.stdin.write(image.tobytes())
+
     def stop(self):
         print("Stopping mock drone...")
         self.shut_event.set()
@@ -70,10 +98,36 @@ class mock_drone:
         if self.thread.is_alive():
             print("Waiting for drone thread")
             self.thread.join()
+
         self.socket.close()
+        self.cap.release()
+        self.proc.stdin.close()
+        self.proc.wait()
+        print("Stopped video.")
         print("Successfully stopped mock drone")
 
     def run(self):
+        import subprocess
+
+        ffmpeg_cmd = [
+            "ffmpeg",
+            "-y",
+            "-f", "rawvideo",
+            "-pix_fmt", "bgr24",
+            "-s", f"{WIDTH}x{HEIGHT}",
+            "-r", str(FPS),
+            "-i", "-",                 # read from stdin
+            "-vcodec", "libx264",
+            "-preset", "veryfast",
+            "-tune", "zerolatency",
+            "-pix_fmt", "yuv420p",
+            "-x264-params", "keyint=30:scenecut=0:repeat-headers=1",
+            "-f", "h264",
+            f"udp://{SEND_ADDRESS}:{IMAGE_PORT}"
+        ]
+
+        self.proc = subprocess.Popen(ffmpeg_cmd, stdin=subprocess.PIPE)
+
         def wrap():
             from time import sleep
 
@@ -81,16 +135,24 @@ class mock_drone:
                 try:
                     data, a = self.socket.recvfrom(BUFFER_SIZE)
 
-                    if not data or data == "command" or data == "streamon":
+                    if not data:
                         continue
 
                     data = data.decode(encoding="utf-8").split(" ")
-                    
+
+                    print(f"Drone recieved '{data[0]}'")
+
+                    if data[0] == "streamon":
+                        self.streamOn = True
+                        continue
+
+                    if data[0] == "command":
+                        continue
+  
                     if (data[0] == "takeoff" or data[0] == "land"):
                         data.append(20)
 
                     data[1] = float(data[1])
-                    print(f"Drone recieved '{data[0]} {data[1]}'")
 
                     if (data[0] in REVERSE_FUNCTIONS):
                         data[1] *= -1
@@ -101,6 +163,8 @@ class mock_drone:
                     pass
 
                 self.send_data()
+                self.send_image()
+
                 sleep(1 / IPS)
         
         self.thread = Thread(target=wrap)
