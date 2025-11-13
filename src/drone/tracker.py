e2d350a605a2e981de1c410ddecebace5ea5e224
Trym Borg <trym.w.borg@gmail.com>
2025-11-13 19:15:31 +0100

    Added PID controllers for each axis

--- src/drone/tracker.py
+++ src/drone/tracker.py
@@ -1,17 +1,20 @@
-from math import sqrt, pow
+from drone.PID_controller import PID_controller, deadzone, angle_error
 
-DX_DY_CLAMPS = (-10, 10)
-SCALAR = 5
-
-HEIGHT_OFFSET = 200 # px
 FACE_OPTIMAL_SIZE = (320, 320) # 50cm
 FACE_ASSUMED_DISTANCE = 50 # cm
 OPTIMAL_AREA = 102400
 
+FACE_WANTED_DISTANCE = 200
+
 class tracker:
-    def __init__(self, screen_size, drone):
+    def __init__(self, screen_size, drone, dt):
         self.t_width, self.t_height = screen_size
         self.drone = drone
+
+        self.x_contr = PID_controller(0.04, 0.1, dt) # left right
+        self.y_contr = PID_controller(0.2, 0.01, dt) # for back
+        self.z_contr = PID_controller(0.05, 0.01, dt) # up down
+        self.yaw_contr = PID_controller(4, 0.01, dt, setpoint=FACE_WANTED_DISTANCE)
 
     def get_center_of_object(self, obj):
         """
@@ -24,10 +27,10 @@ class tracker:
         """
         
         """
-        screen_middle = (self.t_width // 2, (self.t_height // 2) + HEIGHT_OFFSET)
+        screen_middle = (self.t_width // 2, (self.t_height // 2))
 
-        dx = max(min((screen_middle[0] - point[0]) // SCALAR, DX_DY_CLAMPS[1]), DX_DY_CLAMPS[0])
-        dy = max(min((screen_middle[1] - point[1]) // SCALAR, DX_DY_CLAMPS[1]), DX_DY_CLAMPS[0])
+        dx = screen_middle[0] - point[0]
+        dy = screen_middle[1] - point[1]
 
         return (dx, dy)
     
@@ -40,8 +43,8 @@ class tracker:
         area = w * h
         
         return (OPTIMAL_AREA / area) * FACE_ASSUMED_DISTANCE
-    
-    def run(self, obj):
+
+    def run(self, obj, yaw):
         """
         
         """
@@ -49,7 +52,15 @@ class tracker:
             return
 
         center_point = self.get_center_of_object(obj)
-        dx, dz = self.center_around_point(center_point)
-        dy = (FACE_ASSUMED_DISTANCE - self.get_distance_to_face(obj)) // (SCALAR * 4)
+        dx, dz = self.center_around_point(center_point) # measured face left, right, up and down
+        dy = self.get_distance_to_face(obj) # measured distance to face
+
+        print(dy)
+
+        x_vel = deadzone(self.x_contr.update(dx))
+        y_vel = deadzone(self.y_contr.update(dy))
+        z_vel = deadzone(self.z_contr.update(dz))
+
+        n_yaw = deadzone(self.yaw_contr.update(angle_error(self.yaw_contr.setpoint, yaw)))
         
-        self.drone.rc(-dx, -dy, dz, 0)
+        self.drone.rc(x_vel, -y_vel, -z_vel, n_yaw)
